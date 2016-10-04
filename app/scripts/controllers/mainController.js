787b6dcfe8bce718866edc2ad778d1578accf02c
benscott <ben@benscott.co.uk>
2016-10-04 11:30:44 +0100

    initial commit post scaffolding

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Space3D.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('Space3D')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name Space3D.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('Space3D')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
