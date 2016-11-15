587ada70a459284964a56b2ebb8195e8ed762aad
benscott <ben@benscott.co.uk>
2016-11-15 20:49:24 +0000

    More fixes for heroku

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 var express = require('express'),
     app = express();
 
-app.use(express.static('www'));
+// Authenticator
+app.use(function(req, res, next) {
+    var auth;
+
+    // check whether an autorization header was send    
+    if (req.headers.authorization) {
+      // only accepting basic auth, so:
+      // * cut the starting "Basic " from the header
+      // * decode the base64 encoded username:password
+      // * split the string at the colon
+      // -> should result in an array
+      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+    }
+
+    if (!auth || auth[0] !== 'test' || auth[1] !== 'space3d') {
+        // any of the tests failed
+        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+        res.statusCode = 401;
+        // MyRealmName can be changed to anything, will be prompted to the user
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        // this will displayed in the browser when authorization is cancelled
+        res.end('Unauthorized');
+    } else {
+        // continue with processing, user was authenticated
+        app.use(express.static('www'));
+        next();
+    }
+});
 
 // CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
 app.all('*', function(req, res, next) {
@@ -10,11 +37,10 @@ app.all('*', function(req, res, next) {
     next();
 });
 
-// API Routes
-// app.get('/blah', routeHandler);
-
 app.set('port', process.env.PORT || 5000);
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+
